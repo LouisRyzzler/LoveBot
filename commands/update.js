0c66659cd3237273f847ecd7b4a27f2b4c2d4ca0
LouisRyzzler <louis.langrene@gmail.com>
2021-07-24 14:58:24 +0200

    update

--- commands/update.js
+++ commands/update.js
@@ -31,7 +31,7 @@ class Update {
                         new MessageEmbed()
                             .setTitle("1.0.3")
                             .setColor("#f16179")
-                            .setDescription("<:update:863386248734179338> <:pointer:863335546751287306> __Update :__ \n\nSorti de la commande ``help`. Exécutable n'importe où sur le serveur mais de préférence dans le salon <#862785192628060171> et tapez ``!help``.")
+                            .setDescription("<:update:863386248734179338> <:pointer:863335546751287306> __Update :__ \n\nSorti de la commande ``help``. Exécutable n'importe où sur le serveur mais de préférence dans le salon <#862785192628060171> et tapez ``!help``.")
                     )
                 }
             }    
