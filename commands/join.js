83acffdd9e2a42ae1415b4efb4fb2aa2b2cf27b8
LouisRyzzler <louis.langrene@gmail.com>
2021-07-25 16:50:54 +0200

    join

--- commands/join.js
+++ commands/join.js
@@ -1,11 +1,12 @@
 const { MessageEmbed, MessageAttachment } = require("discord.js");
 
 module.exports = (client) => {
-    client.channels.cache.get('862785768870641704').send(
-        new MessageEmbed()
+    const embed = new MessageEmbed()
         .setAuthor(this.message.author.username, this.message.author.avatarURL())
         .setColor("#35f92")
         .setFooter("Un utilisateur a rejoins")
-        .setTimestamp()
-    )
+        .setTimestamp();
+
+    client.channels.cache.get('862785768870641704').send(embed)
+    
 }
