eaa74211f9b607d28d6fd3d732a59f676dc3aafd
LouisRyzzler <louis.langrene@gmail.com>
2021-07-25 16:39:25 +0200

    join

--- commands/join.js
+++ commands/join.js
@@ -1,37 +1,11 @@
 const { MessageEmbed, MessageAttachment } = require("discord.js");
-const { PREFIX } = require("../config");
 
-class Join  {
-    constructor(message, client) {
-        this.client = client;
-        this.message = message;
-        this.args = message.content.slice().split(/ /).filter(element => element !== '');
-    }
-
-
-    selector() {
-        if(this.message.author.id !== this.client.user.id) {
-            switch(this.args[0]) {
-                case PREFIX + "join":
-                    this.Join(); 
-                    break; 
-            }
-        }
-    }
-
-
-
-    Join() {
-        this.client.channels.cache.get('862785768870641704').send(
-            new MessageEmbed()
-            .setAuthor(this.message.author.username, this.message.author.avatarURL())
-            .setColor("#35f92")
-            .setFooter("Un utilisateur a rejoins")
-            .setTimestamp()
-        )
-    }
-}
-
-module.exports ={
-    Join 
+module.exports = (client) => {
+    client.channels.cache.get('862785768870641704').send(
+        new MessageEmbed()
+        .setAuthor(this.message.author.username, this.message.author.avatarURL())
+        .setColor("#35f92")
+        .setFooter("Un utilisateur a rejoins")
+        .setTimestamp()
+    )
 }
