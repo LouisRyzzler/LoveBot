35b5c662f24d34b96dd46c021ce190054c8ae04f
LouisRyzzler <louis.langrene@gmail.com>
2021-07-25 15:42:32 +0200

    help

--- commands/help.js
+++ commands/help.js
@@ -33,7 +33,11 @@ class Help {
                         .setColor("#f16179")
                         .attachFiles(exclamation)
                         .setThumbnail('attachment://exclamation.png')
-                        .setDescription("**Prefix**\n Mon prefix sur Sweety est: ``!``\n\n Pour entrer une commande sur le serveur il vous suffit d'écrire le prefix puis le nom de la commande, *suivre le model ci-contre :* ``!help``\n\n **Commandes**\n <:pointer:863335546751287306> ``!rencontre`` si tapé dans le salon <#862784305439834142> cela vous permettra de poster votre profil.\n <:pointer:863335546751287306> ``!avis`` si tapé dans le salon <#862784628980187157> cela vous permettra de poster votre avis. \n <:pointer:863335546751287306> ``!casino`` si tapé dans le salon <#868776883284758619> vous permettra de jourer au casino. Si vous tomber sur ``7 7 7`` vous gagnerez un rôle personnalisé. \n <:pointer:863335546751287306> ``!dice`` si tapé dans le salon <#868776883284758619> vous ppermettra de jouer aux dés. Si vous tomber sur ``0`` vous gagnerez un Nitro Classic.\n\n **Liens utiles**\n <:pointer:863335546751287306> Invitation du serveur: https://discord.gg/c56RxVhfr\n <:pointer:863335546751287306> Serveur secondaire: https://discord.gg/u8juXJapRQ")
+                        .addFields(
+                            { name: "Prefix", value: "Mon prefix sur Sweety est: ``!`` \n\nPour entrer une commande sur le serveur il vous suffit d'écrire le prefix puis le nom de la commande, *suivre le model ci-contre :* ``!help``", inline: false },
+                            { name: "Commande", value: "<:pointer:863335546751287306> ``!rencontre`` si tapé dans le salon <#862784305439834142> cela vous permettra de poster votre profil.\n <:pointer:863335546751287306> ``!avis`` si tapé dans le salon <#862784628980187157> cela vous permettra de poster votre avis.\n <:pointer:863335546751287306> ``!casino`` si tapé dans le salon <#868776883284758619> vous permettra de jourer au casino. Si vous tomber sur ``7 7 7`` vous gagnerez un rôle personnalisé.\n <:pointer:863335546751287306> !dice si tapé dans le salon <#868776883284758619> vous ppermettra de jouer aux dés. Si vous tomber sur ``0`` vous gagnerez un Nitro Classic.", inline: false},
+                            { name: "Lien utile", value: "<:pointer:863335546751287306> Invitation du serveur: https://discord.gg/c56RxVhfr\n <:pointer:863335546751287306> Serveur secondaire: https://discord.gg/u8juXJapRQ", inline: false }
+                        )
                 )
             }
     }
