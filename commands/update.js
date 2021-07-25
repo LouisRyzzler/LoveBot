bd3f92955fb4ec8f45465329dcb096439b2b5475
LouisRyzzler <louis.langrene@gmail.com>
2021-07-25 12:59:37 +0200

    update

--- commands/update.js
+++ commands/update.js
@@ -32,7 +32,17 @@ class Update {
                             .setTitle("1.0.4")
                             .setColor("#f16179")
                             .setDescription("<:update:863386248734179338> <:pointer:863335546751287306> __Update :__ \n\nSorti de la commande ``casino``. Exécutable dans le salon <#868776883284758619> tapez ``!casino``. Si le casino est remporté: ``7 7 7``, vous gagnez un **rôle personnalisé**, toutes demandes se feront auprès du staff.\n\n Sorti de la commande ``dice``. Exécutable dans le salon <#868776883284758619> tapez ``!dice``. Si le dé est égal à: ``0``, vous gagnez un **Nitro Classic**, toutes demandes se feront auprès du staff.\n\nRetrait de la réaction ``❌`` dislike, pour la commande ``!rencontre``.")
-                    )
+                    ).then(async (message) => {
+                        await message.react('<:update:863386248734179338>').then().catch(console.error);
+
+                        const filter = (reaction, user) => reaction.emoji.name === "<:update:863386248734179338>" &&
+                            user.id === this.message.author.id;
+
+                        await message.awaitReactions(filter, {
+                            max: 1,
+                            errors: ["max"]
+                        })
+                    })
                 }
             }    
         }
