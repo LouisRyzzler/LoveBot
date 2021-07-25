3ce07561d4f61df4c774086d3f9622bdb3fb922b
LouisRyzzler <louis.langrene@gmail.com>
2021-07-25 12:01:10 +0200

    dice

---
+++ commands/dice.js
@@ -0,0 +1,50 @@
+const { MessageEmbed, MessageAttachment } = require("discord.js");
+const diceImg = new MessageAttachment('./assets/img/dice.png');
+const randomDice = () => Math.floor(Math.random() * 2000 ) + 1;
+const { PREFIX } = require("../config");
+
+class Casino {
+    constructor(message, client) {
+        this.client = client;
+        this.message = message;
+        this.args = message.content.slice().split(/ /).filter(element => element !== '');
+    }
+
+
+    selector() {
+        if(this.message.author.id !== this.client.user.id) {
+            switch(this.args[0]) {
+                case PREFIX + "dice":
+                    this.Dice(); 
+                    break; 
+            }
+        }
+    }
+
+
+
+    Dice() {
+        if(this.message.channel.id !== this.client.user.id) {
+            if(this.message.channel.id === "868776883284758619") { 
+                
+                this.message.delete().then().catch(console.error)
+                if(this.args[0] === PREFIX + "dice") {
+                    this.message.channel.send(
+                        new MessageEmbed()
+                            .setAuthor(this.message.author.username, this.message.author.avatarURL())
+                            .setColor("#f16179")
+                            .attachFiles(diceImg)
+                            .setThumbnail('attachment://dice.png')
+                            .addFields(
+                                { name: '#', value: randomDice(), inline: true }
+                            )
+                    )
+                }
+            }    
+        }
+    }
+}
+
+module.exports ={
+    Casino
+}
--- commands/update.js
+++ commands/update.js
@@ -31,7 +31,7 @@ class Update {
                         new MessageEmbed()
                             .setTitle("1.0.4")
                             .setColor("#f16179")
-                            .setDescription("<:update:863386248734179338> <:pointer:863335546751287306> __Update :__ \n\nSorti de la commande ``casino``. Exécutable dans le salon <#868776883284758619> tapez ``!casino``. Si le casino est remporté: ```7 7 7``, vous gagnez un rôle personnalisé, toutes demandes se feront auprès du staff.\n\nRetrait de la réaction ``❌`` dislike, pour la commande ``!rencontre``.")
+                            .setDescription("<:update:863386248734179338> <:pointer:863335546751287306> __Update :__ \n\nSorti de la commande ``casino``. Exécutable dans le salon <#868776883284758619> tapez ``!casino``. Si le casino est remporté: ``7 7 7``, vous gagnez un **rôle personnalisé**, toutes demandes se feront auprès du staff.\n\n Sorti de la commande ``dice``. Exécutable dans le salon <#868776883284758619> tapez ``!dice``. Si le dé est égal à: ``0``, vous gagnez un **Nitro Classic**, toutes demandes se feront auprès du staff.\n\nRetrait de la réaction ``❌`` dislike, pour la commande ``!rencontre``.")
                     )
                 }
             }    
