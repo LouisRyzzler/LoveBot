c05f785f9bf5e2b6602193a5ab6b342ef0afb4eb
LouisRyzzler <louis.langrene@gmail.com>
2021-07-25 22:04:25 +0200

    help

--- commands/help.js
+++ commands/help.js
@@ -36,6 +36,7 @@ class Help {
                         .addFields(
                             { name: "Prefix", value: "Mon prefix sur Sweety est: ``!`` \n\nPour entrer une commande sur le serveur il vous suffit d'écrire le prefix puis le nom de la commande, *suivre le model ci-contre :* ``!help``", inline: false },
                             { name: "Commandes", value: "<:pointer:863335546751287306> ``!rencontre`` si tapé dans le salon <#862784305439834142> cela vous permettra de poster votre profil.\n <:pointer:863335546751287306> ``!avis`` si tapé dans le salon <#862784628980187157> cela vous permettra de poster votre avis.\n <:pointer:863335546751287306> ``!casino`` si tapé dans le salon <#868776883284758619> vous permettra de jourer au casino. Si vous tomber sur ``7 7 7`` vous gagnerez un rôle personnalisé.\n <:pointer:863335546751287306> !dice si tapé dans le salon <#868776883284758619> vous ppermettra de jouer aux dés. Si vous tomber sur ``0`` vous gagnerez un Nitro Classic.", inline: false},
+                            { name: "Rôles", value: "<:pointer:863335546751287306> ``!gold`` ce rôle s'obtient en invitant ``3`` personnes sur le serveur. Taper cette commande vous montreras tous les avantages de ce rôle."},
                             { name: "Liens utiles", value: "<:pointer:863335546751287306> Invitation du serveur: https://discord.gg/c56RxVhfr\n <:pointer:863335546751287306> Serveur secondaire: https://discord.gg/u8juXJapRQ", inline: false }
                         )
                 )
