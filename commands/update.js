2eb44bd88f59ceac2dca3f5a29376aaf928a7ea7
LouisRyzzler <louis.langrene@gmail.com>
2021-07-26 19:47:50 +0200

    update

--- commands/update.js
+++ commands/update.js
@@ -31,7 +31,7 @@ class Update {
                         new MessageEmbed()
                             .setTitle("1.0.5")
                             .setColor("#f16179")
-                            .setDescription("<:update:863386248734179338> <:pointer:863335546751287306> __Update :__ \n\nRôle `Sweety Gold`. Pour l'obtenir invitez `3` personnes, tapez la commande `!gold` pour voir tous les avantages de ce rôle. \n\nRedisgn de la commande `!help` et `info rencontre` <#862784305439834142>.")
+                            .setDescription("<:update:863386248734179338> <:pointer:863335546751287306> __Update :__ \n\n Sorti de la commande `!helpcasino`, ainsi vous aurez toutes les infos concernant le casino.\n\nRôle `Boost`. Pour l'obtenir boostez le serveur au moins uen fois. \n\nRe-redisgn de la commande `!help`.")
                     ).then(async (message) => {
                         await message.react('<:update:863386248734179338>').then().catch(console.error);
 
