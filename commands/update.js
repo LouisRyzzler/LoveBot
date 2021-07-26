818c93908a331203e7d24eeca3f680f290313c25
LouisRyzzler <louis.langrene@gmail.com>
2021-07-26 09:05:13 +0200

    update

--- commands/update.js
+++ commands/update.js
@@ -31,7 +31,7 @@ class Update {
                         new MessageEmbed()
                             .setTitle("1.0.4")
                             .setColor("#f16179")
-                            .setDescription("<:update:863386248734179338> <:pointer:863335546751287306> __Update :__ \n\nSorti de la commande ``casino``. Exécutable dans le salon <#868776883284758619> tapez ``!casino``. Si le casino est remporté: ``7 7 7``, vous gagnez un **rôle personnalisé**, toutes demandes se feront auprès du staff.\n\n Sorti de la commande ``dice``. Exécutable dans le salon <#868776883284758619> tapez ``!dice``. Si le dé est égal à: ``0``, vous gagnez un **Nitro Classic**, toutes demandes se feront auprès du staff.\n\nRetrait de la réaction ``❌`` dislike, pour la commande ``!rencontre``.")
+                            .setDescription("<:update:863386248734179338> <:pointer:863335546751287306> __Update :__ \n\nRôle `Sweety Gold`. Pour l'obtenir invitez `3` personnes, tapez la commande `!gold` pour voir tous les avantages de ce rôle. \n\nRedisgn de la commande `!help` et `info rencontre` <#862784305439834142>.")
                     ).then(async (message) => {
                         await message.react('<:update:863386248734179338>').then().catch(console.error);
 
