bde793da1f4578a566efd7ab84f753bcd7fac571
LouisRyzzler <louis.langrene@gmail.com>
2021-07-26 17:04:38 +0200

    help

--- commands/helpcasino.js
+++ commands/helpcasino.js
@@ -30,10 +30,10 @@ class Helpcasino {
                     new MessageEmbed()
                         .setColor("#f16179")
                         .addFields(
-                            { name: "Casino", value: "Pour jouer au casino, allez dans le channel <#868776883284758619> et tapez la commande `!casino`.", inline: false },
+                            { name: "Casino", value: "Pour jouer au casino, allez dans le channel <#868776883284758619> et tapez la commande `!casino`.", inline: true },
                             { name: "Comment ganger ?", value: "Pour ganger au jeu du casino, il vous suffit d'obtenir la suite de numéro `7 7 7`. En récompense un rôle personnalisé vous sera attribué, toutes demadnes est à faire auprès d'un administateur.", inline: true},
                             { name: "Les dés", value: "Pour jouer aux dés, allez dans le channel <#868776883284758619> et tapez la commande `!dice`.", inline: false},
-                            { name: "Comment gagner aux dés ?", value: "Pour gagner aux dés, il vous suffit d'obtenir le numéro `0`. En récompense un **Nitro Claasic** vous sera remis sous reserve d'une preuve.", inline: false, inline: true}
+                            { name: "Comment gagner aux dés ?", value: "Pour gagner aux dés, il vous suffit d'obtenir le numéro `0`. En récompense un **Nitro Classic** vous sera remis sous reserve d'une preuve.", inline: false, inline: true}
                         )
                         .setFooter(this.message.author.username, this.message.author.avatarURL())
                 )
