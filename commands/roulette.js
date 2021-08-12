5b462aad4ab3b898ebbe4e8ddf9ebaa69c689ffc
LouisRyzzler <louis.langrene@gmail.com>
2021-08-12 21:26:07 +0200

    roulette

--- commands/roulette.js
+++ commands/roulette.js
@@ -1,9 +1,9 @@
 const { MessageEmbed, MessageAttachment } = require("discord.js");
 const diceImg = new MessageAttachment('./assets/img/dice.png');
-const randomDice = () => Math.floor(Math.random() * 2 ) + 1;
+const randomDice = () => Math.floor(Math.random() * 2000 ) + 1;
 const { PREFIX } = require("../config");
 
-class Roulette{
+class Roulette {
     constructor(message, client) {
         this.client = client;
         this.message = message;
@@ -27,57 +27,51 @@ class Roulette{
         if(this.message.channel.id !== this.client.user.id) {
             if(this.message.channel.id === "868776883284758619") { 
 
-                if(randomDice === 1 ) {
                     this.message.delete().then().catch(console.error)
                     if(this.args[0] === PREFIX + "roulette") {
-                        this.message.channel.send(
-                            new MessageEmbed()
-                                .setAuthor(this.message.author.username, this.message.author.avatarURL())
-                                .setColor("#f16179")
-                                .attachFiles(diceImg)
-                                .setThumbnail('attachment://dice.png')
-                                .addFields(
-                                    { name: '#', value: 'ça marche correctement =1', inline: true }
-                                )
-                                .setFooter("Tape !dice pour jouer.")
-                        )
-                    }
-                }
 
-                if(randomDice === 2) {
-                    this.message.delete().then().catch(console.error)
-                    if(this.args[0] === PREFIX + "dice") {
-                        this.message.channel.send(
-                            new MessageEmbed()
-                                .setAuthor(this.message.author.username, this.message.author.avatarURL())
-                                .setColor("#f16179")
-                                .attachFiles(diceImg)
-                                .setThumbnail('attachment://dice.png')
-                                .addFields(
-                                    { name: '#', value: 'ça marche correctement =2', inline: true }
-                                )
-                                .setFooter("Tape !dice pour jouer.")
-                        )
-                    }
-                }
+                        if(randomDice === 1) {
+                            this.message.channel.send(
+                                new MessageEmbed()
+                                    .setAuthor(this.message.author.username, this.message.author.avatarURL())
+                                    .setColor("#f16179")
+                                    .attachFiles(diceImg)
+                                    .setThumbnail('attachment://dice.png')
+                                    .addFields(
+                                        { name: '#', value: 'égale à 1', inline: true }
+                                    )
+                                    .setFooter("Tape !roulette pour jouer.")
+                            )
+                        }
 
-                if(randomDice === 3) {
-                    this.message.delete().then().catch(console.error)
-                    if(this.args[0] === PREFIX + "dice") {
-                        this.message.channel.send(
-                            new MessageEmbed()
-                                .setAuthor(this.message.author.username, this.message.author.avatarURL())
-                                .setColor("#f16179")
-                                .attachFiles(diceImg)
-                                .setThumbnail('attachment://dice.png')
-                                .addFields(
-                                    { name: '#', value:  'ya une 3eme possibilitée', inline: true }
-                                )
-                                .setFooter("Tape !dice pour jouer.")
-                        )
-                    }
-                }
+                        if(randomDice === 2) {
+                            this.message.channel.send(
+                                new MessageEmbed()
+                                    .setAuthor(this.message.author.username, this.message.author.avatarURL())
+                                    .setColor("#f16179")
+                                    .attachFiles(diceImg)
+                                    .setThumbnail('attachment://dice.png')
+                                    .addFields(
+                                        { name: '#', value: 'égale à 2', inline: true }
+                                    )
+                                    .setFooter("Tape !roulette pour jouer.")
+                            )
+                        }
 
+                        if(randomDice === 3) {
+                            this.message.channel.send(
+                                new MessageEmbed()
+                                    .setAuthor(this.message.author.username, this.message.author.avatarURL())
+                                    .setColor("#f16179")
+                                    .attachFiles(diceImg)
+                                    .setThumbnail('attachment://dice.png')
+                                    .addFields(
+                                        { name: '#', value: 'jai chié', inline: true }
+                                    )
+                                    .setFooter("Tape !roulette pour jouer.")
+                            )
+                        }
+                    }   
             }    
         }
     }
