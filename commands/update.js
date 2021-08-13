c65238221fd0a283afb6bc64f6cf94ca1cc15d38
LouisRyzzler <louis.langrene@gmail.com>
2021-08-13 14:00:04 +0200

    update

--- commands/update.js
+++ commands/update.js
@@ -29,7 +29,7 @@ class Update {
                 if(this.args[0] === PREFIX + "update") {
                     this.message.channel.send(
                         new MessageEmbed()
-                            .setTitle("1.0.7")
+                            .setTitle("1.0.8")
                             .setColor("#f16179")
                             .setDescription("<:update:863386248734179338> <:pointer:863335546751287306> __Update :__ Sorti de la commande ```!roulette``` pouvant être utiliser dans le salon <#868776883284758619>. \n Un jeu ù il vous suffit de miser sur une couleur (rouge ou bleu). \n\n La récompense pour le vainqueurs reste indéterminé.")
                     ).then(async (message) => {
