03c48a969ecd863030bc75d48c82e7877038b875
LouisRyzzler <louis.langrene@gmail.com>
2021-08-14 00:08:16 +0200

    casino

--- commands/casino.js
+++ commands/casino.js
@@ -30,7 +30,7 @@ class Casino {
                 this.message.delete().then().catch(console.error)
                 if(this.args[0] === PREFIX + "casino") {
                     function sendCasino(chanel) {
-                        channel.send(
+                        this.channel.send(
                         new MessageEmbed()
                             .setAuthor(this.message.author.username, this.message.author.avatarURL())
                             .setColor("#f16179")
