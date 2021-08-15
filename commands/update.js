cd5eaabf01d184e768f0cf3ca5839e479c7cf86e
LouisRyzzler <louis.langrene@gmail.com>
2021-08-15 21:00:39 +0200

    update

--- commands/update.js
+++ commands/update.js
@@ -29,9 +29,9 @@ class Update {
                 if(this.args[0] === PREFIX + "update") {
                     this.message.channel.send(
                         new MessageEmbed()
-                            .setTitle("1.0.8")
+                            .setTitle("2.0.0")
                             .setColor("#f16179")
-                            .setDescription("<:update:863386248734179338> <:pointer:876532736435826729> __Update :__ \n\n réagencement de tout le serveur laissant place à un Sweety2 + ajustement du bot en fonction de la détection de salons.")
+                            .setDescription("<:update:863386248734179338> <:pointer:876532736435826729> __Update :__ \n\n Réagencement de tout le serveur laissant place à un Sweety2 + ajustement du bot en fonction de la détection de salons.")
                     ).then(async (message) => {
                         await message.react('<:update:863386248734179338>').then().catch(console.error);
 
