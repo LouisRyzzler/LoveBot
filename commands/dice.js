cac363b83dca34bfae615ac1cfae4f4529036fa0
LouisRyzzler <louis.langrene@gmail.com>
2021-08-16 22:54:19 +0200

    dice

--- commands/dice.js
+++ commands/dice.js
@@ -1,6 +1,7 @@
 const { MessageEmbed, MessageAttachment } = require("discord.js");
 const diceImg = new MessageAttachment('./assets/img/dice.png');
-const randomDice = () => Math.floor(Math.random() * 2000 ) + 1;
+const nitro = new MessageAttachment('./assets/img/nitro.png');
+const randomDice = () => Math.floor(Math.random() * 2 ) + 1;
 const { PREFIX } = require("../config");
 
 class Dice {
@@ -40,6 +41,19 @@ class Dice {
                             )
                             .setFooter("Tape !dice pour jouer.")
                     )
+                    if( randomDice() = 1) {
+                        this.message.channel.send(
+                            new MessageEmbed()
+                            .setAuthor(this.message.author.username, this.message.author.avatarURL())
+                            .setColor("#f16179")
+                            .attachFiles(nitro)
+                            .setThumbnail('attachment://nitro.png')
+                            .addFields(
+                                { name: 'Tu as gagné', value: "Félicitation ! Tu viens de remporter un Nitro Classic. \nFait ta demande auprès de <@396425830604996610>.", inline: true }
+                            )
+                            .setFooter("Tape !dice pour jouer.")
+                        )
+                    }
                 }
             }    
         }
