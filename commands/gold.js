253e09cda2d9c16a9bb1734a6c38c91dd5a5da8f
LouisRyzzler <louis.langrene@gmail.com>
2021-08-29 21:27:53 +0200

    gold

--- commands/gold.js
+++ commands/gold.js
@@ -33,7 +33,7 @@ class Gold {
                         .attachFiles(boost2)
                         .setThumbnail('attachment://Boost2.gif')
                         .addFields(
-                            { name: "Comment obtenir le rôle SweetyGold ?", value: "Inviter **3 personnes** sur le serveur vous permettra d'obtenir le rôle <@&876526738174255134>", inline: false },
+                            { name: "Comment obtenir le rôle MembreGold ?", value: "Inviter **3 personnes** sur le serveur vous permettra d'obtenir le rôle <@&876526738174255134>", inline: false },
                             { name: "Avantages", value: "<:pointer:876532736435826729> Vous aurez un rôle affiché séparément des autres \n<:pointer:876532736435826729> Vous pourrez changer votre pseudo. \n<:pointer:876532736435826729> Vous pourrez poster des GIF dans le salon <#834362034392924211>. \n<:pointer:876532736435826729> Vous aurez la possibilité de masquer le channel <#834555074045804605>. Faites votre demande auprès du staff si vous voulez cacher ce salon. \n\nPour voir où en sont vos invitations, tapez `-invites`." }
                         )
                         .setFooter("Tape !gold pour revoir ce message.")
