2119ddd14b4fb6c38c8b000d12ec31317b053d44
LouisRyzzler <louis.langrene@gmail.com>
2021-08-31 11:36:20 +0200

    start

--- index.js
+++ index.js
@@ -66,11 +66,12 @@ client.on('guildMemberAdd', async( member ) => {
         .attachFiles(gif);
 
     const channel = member.guild.channels.cache.get(channelId)
-    channel.send(embed)
+    channel.send(embed);
+});
 
-    var role = member.guild.roles.find("id", "881598134856323112");
-    member.addRole(role);
-})
+client.on('guildMemberAdd', ( guildMember ) => {
+    guildMember.addRole(guildMember.guild.roles.find(role => role.name === "Membre"));
+});
 
 
 
