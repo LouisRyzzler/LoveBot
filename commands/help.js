c71be991d4b0a920ecc1e9a7bfe37f7058e92fa7
LouisRyzzler <louis.langrene@gmail.com>
2021-11-08 21:55:46 +0100

    aide

--- commands/help.js
+++ commands/help.js
@@ -33,7 +33,7 @@ class Help {
                         .attachFiles(exclamation)
                         .setThumbnail('attachment://exclamation.png')
                         .addFields(
-                            { name: "Prefix", value: "Mon prefix sur Rencontres FR est: **!** \n\nPour entrer une commande sur le serveur il vous suffit d'écrire le prefix puis le nom de la commande, *suivre le model ci-contre :* **!help**", inline: false },
+                            { name: "Prefix", value: "Mon prefix sur 🌌 l'Univers est: **!** \n\nPour entrer une commande sur le serveur il vous suffit d'écrire le prefix puis le nom de la commande, *suivre le model ci-contre :* **!help**", inline: false },
                             { name: "Commandes Membres", value: "<:pointer:876532736435826729> `!avis`\n\n <:pointer:876532736435826729> `!helpcasino` \n<:pointer:876532736435826729> `!casino`\n <:pointer:876532736435826729> `!dice`\n <:pointer:876532736435826729> `!roulette`", inline: false},
                             { name: "Commandes Rôles", value: "<:pointer:876532736435826729> `!gold` \n<:pointer:876532736435826729> `!boost`"},
                             { name: "Liens utiles", value: "<:pointer:876532736435826729> Invitation du serveur: https://discord.gg/zYm5s97HtF", inline: false }
