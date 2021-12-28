ad1d7544ddb16e0b6240c9e8b2f82d48de605c78
LouisRyzzler <louis.langrene@gmail.com>
2021-12-28 21:25:42 +0100

    fregr

--- commands/help.js
+++ commands/help.js
@@ -34,8 +34,8 @@ class Help {
                         .setThumbnail('attachment://exclamation.png')
                         .addFields(
                             { name: "Prefix", value: "Mon prefix sur Sweety est: **!** \n\nPour entrer une commande sur le serveur il vous suffit d'écrire le prefix puis le nom de la commande, *suivre le model ci-contre :* **!help**", inline: false },
-                            { name: "Commandes Membres", value: "<:pointer:882148627282280488> `!avis`           <a:4231grayarrowright:882148688447811594> <#881598135233830963>\n <:pointer:882148627282280488> `!casino` <a:4231grayarrowright:882148688447811594> <#881598135510642780>\n <:pointer:882148627282280488> `!dice` <a:4231grayarrowright:882148688447811594> <#881598135510642780>\n <:pointer:882148627282280488> `!roulette` <a:4231grayarrowright:882148688447811594> <#881598135510642780>", inline: false},
-                            { name: "Commandes Rôles", value: "<:pointer:882148627282280488> `!gold` \n<:pointer:882148627282280488> `!boost`"},
+                            { name: "Commandes Membres", value: "<:pointer:882148627282280488> `!avis` <a:4231grayarrowright:882148688447811594> <#881598135233830963>\n <:pointer:882148627282280488> `!casino` <a:4231grayarrowright:882148688447811594> <#881598135510642780>\n <:pointer:882148627282280488> `!dice` <a:4231grayarrowright:882148688447811594> <#881598135510642780>\n <:pointer:882148627282280488> `!roulette` <a:4231grayarrowright:882148688447811594> <#881598135510642780> \n\n", inline: false},
+                            { name: "Commandes Rôles", value: "<:pointer:882148627282280488> `!gold` \n<:pointer:882148627282280488> `!boost` \n\n"},
                             { name: "Liens utiles", value: "<:pointer:882148627282280488> Invitation du serveur: https://discord.gg/zYm5s97HtF", inline: false }
                         )
                         .setFooter("Tape !help pour revoir ce message.")
