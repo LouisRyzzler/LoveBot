f65edd4d4df5d73f027c380b4e04c5707d871031
LouisRyzzler <louis.langrene@gmail.com>
2021-12-29 14:04:17 +0100

    grger

--- commands/gold.js
+++ commands/gold.js
@@ -34,7 +34,7 @@ class Gold {
                         .setThumbnail('attachment://Boost2.gif')
                         .addFields(
                             { name: "Comment obtenir le rôle MembreGold ?", value: "Inviter **3 personnes** sur le serveur vous permettra d'obtenir le rôle <@&881598134856323114>", inline: false },
-                            { name: "Avantages", value: "<:pointer:876532736435826729> Vous aurez un rôle affiché séparément des autres \n<:pointer:876532736435826729> Vous pourrez changer votre pseudo. \n<:pointer:876532736435826729> Vous pourrez poster des GIF dans le salon <#881598135233830965>. \n<:pointer:876532736435826729> Vous aurez la possibilité de masquer le channel <#881598135510642779>. Faites votre demande auprès du staff si vous voulez cacher ce salon. \n\nPour voir où en sont vos invitations, tapez `-invites`." }
+                            { name: "Avantages", value: "<:pointer:882148627282280488> Vous aurez un rôle affiché séparément des autres \n<:pointer:882148627282280488> Vous pourrez changer votre pseudo. \n<:pointer:882148627282280488> Vous pourrez poster des GIF dans le salon <#881598135233830965>. \n<:pointer:882148627282280488> Vous aurez la possibilité de masquer le channel <#881598135510642779>. Faites votre demande auprès du staff si vous voulez cacher ce salon. \n\nPour voir où en sont vos invitations, tapez `-invites`." }
                         )
                         .setFooter("Tape !gold pour revoir ce message.")
                 )
