457cb2544a7af462a447a97d593d28aa8b9dafa9
LouisRyzzler <louis.langrene@gmail.com>
2021-12-30 13:48:39 +0100

    eger

--- commands/dice.js
+++ commands/dice.js
@@ -1,7 +1,7 @@
 const { MessageEmbed, MessageAttachment } = require("discord.js");
 const diceImg = new MessageAttachment('./assets/img/dice2.png');
 const nitro = new MessageAttachment('./assets/img/nitro.png');
-const randomDice = () => Math.floor(Math.random() * 2 ) + 1;
+const randomDice = () => Math.floor(Math.random() * 2000 ) + 1;
 const { PREFIX } = require("../config");
 
 class Dice {
@@ -49,7 +49,7 @@ class Dice {
                             .attachFiles(nitro)
                             .setThumbnail('attachment://nitro.png')
                             .addFields(
-                                { name: 'TU AS GAGNE !', value: "⌒⌒⌒⌒⌒⌒⌒ \n\n**Félicitation** ! Tu viens de remporter un **Nitro Classic**. \nFait ta demande auprès de <@396425830604996610>.", inline: true }
+                                { name: 'TU AS GAGNE !', value: "\n\n**Félicitation** ! Tu viens de remporter un **Nitro Classic**. \nFait ta demande auprès de <@396425830604996610>.", inline: true }
                             )
                             .setFooter("Tape !dice pour rejouer.")
                         )
